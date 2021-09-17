c6baf20013dc445223aa2e3a07bbe8d9b3a9839e
Trindkr <maria.trindkaer@gmail.com>
2021-09-17 13:07:18 +0200

    Added student class and tests

---
+++ Student/Student.cs
@@ -0,0 +1,39 @@
+﻿using System;
+
+namespace Student
+{
+    public class Student
+    {
+        int id;
+        string givenName, surName;
+        enum Status
+        {
+            New,
+            Active,
+            Dropout,
+            Graduated
+        }
+        DateTime startDate,endDate, graduationDate;
+        
+        public Student(int id, string givenName, string surName, Status staus, DateTime startdate, DateTime endDate, DateTime graduationDate)
+        {
+            this.id = id;
+            this.givenName = givenName;
+            this.surName = surName;
+            this.staus = status;
+            this.startDate = startDate;
+            this.endDate = endDate;
+            this.graduationDate = graduationDate;
+
+        }
+
+        public Status getStudentStatus(DateTime nowDate)
+        {
+
+            return null;
+        }
+
+    }
+
+    
+}
