cbeb74e8e76ae1f4ce7604f9877377828f34a0a7
rambo <garb@itu.dk>
2021-09-23 17:25:59 +0200

    Removed comments

--- Student.Tests/StudentTest.cs
+++ Student.Tests/StudentTest.cs
@@ -60,53 +60,7 @@ namespace Student.Tests
             Assert.Equal(Status.Active, s1.Status);
         }
 
-        // [Fact]
-        // public void Status_isOnlyReadable_New()
-        // {
-        //     var s2 = new Student(2,"ib","karlsen", new DateTime(2008, 5, 1), new DateTime(2010, 3, 1), new DateTime(2010, 3, 1));
-        //     var date1 = new DateTime(2005, 4, 2);
-        //     var date2 = new DateTime(2008, 5, 1); // EDGE
-
-        //     var output1 = s2.getStudentStatusFromDate(date1);
-        //     var output2 = s2.getStudentStatusFromDate(date2);
-
-        //     Assert.Equal(Status.New, output1);
-        //     Assert.NotEqual(Status.New, output2);
-        // }
-
-        // [Fact]
-        // public void Status_isOnlyReadable_Dropout()
-        // {
-        //     var s3 = new Student(3,"henning","olsen", Status.Dropout, new DateTime(2008, 5, 1), new DateTime(2009, 3, 1), new DateTime(2010, 3, 1));
-        //     var date1 = new DateTime(2010, 4, 2);
-        //     var date2 = new DateTime(2011, 4, 2);
-        //     var date3 = new DateTime(2007, 1, 1);
-
-        //     var output1 = s3.getStudentStatusFromDate(date1);
-        //     var output2 = s3.getStudentStatusFromDate(date2);
-        //     var output3 = s3.getStudentStatusFromDate(date3);
-
-        //     Assert.Equal(Status.Dropout, output1);
-        //     Assert.Equal(Status.Dropout, output2);
-        //     Assert.NotEqual(Status.Dropout, output3);
-        // }
-
-        // [Fact]
-        // public void Status_isOnlyReadable_Graduated()
-        // {
-        //     var s4 = new Student(4, "anne","svensen", Status.Active, new DateTime(2008, 5, 1), new DateTime(2010, 3, 1), new DateTime(2010, 3, 1));
-        //     var date1 = new DateTime(2010, 4, 2);
-        //     var date2 = new DateTime(2009, 4, 2);
-        //     var date3 = new DateTime(2010, 3, 1);
-            
-        //     var output1 = s4.getStudentStatusFromDate(date1);
-        //     var output2 = s4.getStudentStatusFromDate(date2);
-        //     var output3 = s4.getStudentStatusFromDate(date3);
-
-        //     Assert.Equal(Status.Graduated, output1);
-        //     Assert.NotEqual(Status.Graduated, output2);
-        //     Assert.Equal(Status.Graduated, output3);
-        // }
+   
 
         [Fact]
         public void Id_SetOnlyOnClassCreated() 
